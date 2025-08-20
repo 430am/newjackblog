d4522f8d99665c206c9602ceef7efde59e368109
Jack <jack.wade@microsoft.com>
2025-08-20 13:37:17 -0400

    making adjustments to content

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-	site_name: 'AstroPress',
-	title: 'AstroPress | Astro and Tailwind CSS',
-	description: 'Boilerplate built with Astro and Tailwind CSS',
-	author: 'Nicolas',
-	locale_region: 'de-en',
+	site_name: 'Plutodyne',
+	title: 'Plutodyne | Jacks Blog',
+	description: 'Personal blog about Azure, technology, and life',
+	author: 'Jack Wade',
+	locale_region: 'us-en',
 	locale: 'en'
 };
